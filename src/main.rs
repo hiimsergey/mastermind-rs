8831bed4f1e295cfbdebd6ec43512aaebd54d01d
serg <91432388+hiimsergey@users.noreply.github.com>
2023-09-08 20:49:30 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -28,7 +28,7 @@ fn about(s: &mut Cursive) {
 | | | | | | (_| \\__ \\ ||  __/ |  | | | | | | | | | | (_| |
 |_| |_| |_|\\__,_|___/\\__\\___|_|  |_| |_| |_|_|_| |_|\\__,_|";
     let description = "
-A silly game written for the sake of experience in writing Rust code. Also my first project using any kind of User Interface.
+A simple game written for the sake of experience in writing Rust code. Also my first project using any kind of User Interface.
 
 ";
     let desc_cursive = "
@@ -39,79 +39,79 @@ Utilizes the \"cursive\" crate for building TUIs.
     let layout = LinearLayout::vertical()
         // TODO check out TextView to probably make this part more efficient
         .child(TextView::new(banner))
-        .child(TextView::new(description))
-        // TODO find up right arrow char
-        .child(Button::new("source ↗", |_| {
+        .child(TextView::new(StyledString::styled(
+            description,
+            Color::Dark(BaseColor::Blue)
+        )))
+        .child(Button::new("source ↗", |s| {
             if !webbrowser::open(
                 "https://github.com/hiimsergey/mastermind-rs"
             ).is_ok() {
-                eprintln!("Failed to open website, sorry :(")
+                source_error(s);
             }
         }))
-        .child(TextView::new(desc_cursive))
-        .child(Button::new("source ↗", |_| {
+        .child(TextView::new(StyledString::styled(
+            desc_cursive,
+            Color::Dark(BaseColor::Blue)
+        )))
+        .child(Button::new("source ↗", |s| {
             if !webbrowser::open(
                 "https://crates.io/crates/cursive"
             ).is_ok() {
-                eprintln!("Failed to open website, sorry :[")
+                source_error(s);
             };
         }));
 
+    // TODO dont forget to impl this for other menu-originating functions
+    s.clear_global_callbacks('q');
+    s.add_global_callback('q', |s| { s.pop_layer(); menu(s) });
+    s.pop_layer();
     s.add_layer(Dialog::around(layout)
-        .button("Ok", |s| { s.pop_layer(); })
+        .button("Ok", |s| { s.pop_layer(); menu(s) })
         .title("About mastermind-rs")
         .fixed_width(64)
     );
 }
 
 fn menu(s: &mut Cursive) {
-    let menu_buttons = LinearLayout::vertical()
-        // TODO map buttons
-        .child(Button::new("Start game", game))
-        .child(Button::new("Options", |s| s.quit()))
-        .child(Button::new("About", |s| about(s)))
-        .child(DummyView)
-        .child(Button::new("Quit", |s| s.quit()));
-    let menu_message = TextView::new(StyledString::styled(
-        "Another one of those\nuseless games...",
-        Color::Dark(BaseColor::Blue)
-    ));
-
-    // TODO add keyboard callbacks
-    // everywhere
+    // TODO test of time
+    // and every other /global_callback/ too
+    s.add_global_callback('q', |s| s.quit());
     s.pop_layer();
     s.add_layer(Dialog::around(LinearLayout::vertical()
-        .child(menu_message)
+        .child(TextView::new(StyledString::styled(
+            "Another one of those useless games...",
+            Color::Dark(BaseColor::Blue)
+        )))
         .child(DummyView)
-        .child(menu_buttons))
+        // TODO now -> EditView
+        .child(TextView::new("Character number"))
+        .child(TextView::new("Password length"))
+        .child(TextView::new("HP/Round limit")))
+        // TODO make all DummyViews larger
         .title("mastermind-rs")
+        .button("About", about)
+        .button("Quit", |s| s.quit())
     );
 }
 
-fn game(s: &mut Cursive) {
-    let game_buttons = LinearLayout::vertical()
-        // TODO map buttons
-        .child(TextView::new("TODO show ingame stats"))
-        .child(DummyView)
-        .child(Button::new("Restart game", menu))
-        .child(Button::new("Back to menu", menu))
-        .child(DummyView)
-        .child(Button::new("Ragequit", |s| s.quit()));
-
-    s.pop_layer();
-    s.add_layer(Dialog::around(LinearLayout::horizontal()
-        .child(TextView::new("TODO example description"))
-        .child(DummyView)
-        .child(game_buttons))
-        .title("game")
+fn source_error(s: &mut Cursive) {
+    s.clear_global_callbacks('q');
+    s.add_global_callback('q', |s| {
+        s.pop_layer();
+        s.add_global_callback('q', menu);
+    });
+    s.add_layer(Dialog::text(
+"Couldn't connect to the website. :[
+Check that you have a web browser installed.")
+        .title("Connection error")
+        .button("Ok", |s| { s.pop_layer(); })
     );
 }
 
 fn main() {
     let mut siv = cursive::default();
-
     menu(&mut siv);
-
     siv.run();
     // TODO change theme
 }
